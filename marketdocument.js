9eb0f383eea0bfe618355350a82338d1c9e97629
niels <niels@handshake-systems.nl>
2025-04-22 13:44:20 +0200

    added

--- marketdocument.js
+++ marketdocument.js
@@ -180,7 +180,9 @@ function buildUnsignedSOAP(bodyXmlBuilder, certificate) {
     'xmlns:soapenv': 'http://schemas.xmlsoap.org/soap/envelope/',
     'xmlns:hub': 'http://sys.svc.tennet.nl/AncillaryServices/',
     'xmlns:wsse': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
-    'xmlns:wsu': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd'
+    'xmlns:wsu': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd',
+    'xmlns:head': 'http://sys.svc.tennet.nl/MMCHub/Header/v1'
+
   });
 
   const header = envelope.ele('soapenv:Header');
