4e303e968fcdb1d98139b5e832f1c34a2caafbec
niels <niels@handshake-systems.nl>
2025-05-20 16:28:41 +0200

    added php version

---
+++ php/run.php
@@ -0,0 +1,42 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use RobRichards\XMLSecLibs\XMLSecurityKey;
+use RobRichards\WsePhp\WSSESoap;
+
+$wsdl = __DIR__ . '/wsdl/your.wsdl';
+$options = ['trace' => 1, 'exceptions' => true];
+$signingCert = __DIR__ . '/certs/smime-covolt-pub_staging.pem';
+$signingKey = __DIR__ . '/certs/smime-covolt-key_staging.key';
+
+// Create a minimal dummy SOAP request (or later call your actual method)
+$request = <<<XML
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+  <soapenv:Header/>
+  <soapenv:Body>
+    <example:Hello xmlns:example="http://example.com">
+      <example:Name>Test</example:Name>
+    </example:Hello>
+  </soapenv:Body>
+</soapenv:Envelope>
+XML;
+
+// Load into DOM
+$doc = new DOMDocument();
+$doc->loadXML($request);
+
+// Create WSSE signer
+$objWSSE = new WSSESoap($doc);
+$objWSSE->addTimestamp();
+
+// Sign using SubjectKeyIdentifier
+$key = new XMLSecurityKey(XMLSecurityKey::RSA_SHA256, ['type'=>'private']);
+$key->loadKey($signingKey, true);
+
+// Attach the certificate using SubjectKeyIdentifier
+$objWSSE->signSoapDoc($key);
+$objWSSE->add509Cert(file_get_contents($signingCert), true, false, ['subjectKeyIdentifier' => true]);
+
+// Output the signed XML
+echo $objWSSE->saveXML();
