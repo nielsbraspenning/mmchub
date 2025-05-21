0a77d7eff17f94b085c6c44b124880322c1cf8e2
niels <niels@handshake-systems.nl>
2025-05-21 16:12:26 +0200

    back to older version

--- php/run.php
+++ php/run.php
@@ -3,8 +3,9 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use RobRichards\XMLSecLibs\XMLSecurityKey;
-use RobRichards\XMLSecLibs\XMLSecurityDSig;
 use RobRichards\WsePhp\WSSESoap;
+use RobRichards\XMLSecLibs\XMLSecurityDSig;
+
 
 // === CONFIG ===
 $signingCert = __DIR__ . '/certs/smime-covolt-pub_staging.pem';
@@ -14,7 +15,7 @@ $signingKey  = __DIR__ . '/certs/smime-covolt-key_staging.key';
 $request = <<<XML
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
   <soapenv:Header>
-    <MessageAddressing xmlns="http://sys.svc.tennet.nl/MMCHub/Header/v1">
+      <MessageAddressing xmlns="http://sys.svc.tennet.nl/MMCHub/Header/v1">
       <technicalMessageId>ee1a1b0a-ec89-446a-91aa-15abc0fd4b7a</technicalMessageId>
       <correlationId>ee1a1b0a-ec89-446a-91aa-15abc0fd4b7a</correlationId>
       <senderId>8719333027500</senderId>
@@ -23,7 +24,7 @@ $request = <<<XML
       <contentType>ACTIVATED_FCR</contentType>
     </MessageAddressing>
   </soapenv:Header>
-  <soapenv:Body xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+  <soapenv:Body xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" wsu:Id="Body">
     <example:Hello xmlns:example="http://example.com">
       <example:Name>Test</example:Name>
     </example:Hello>
@@ -39,46 +40,13 @@ $doc->loadXML($request);
 $objWSSE = new WSSESoap($doc);
 $objWSSE->addTimestamp();
 
-// === XPath helper ===
-$xpath = new DOMXPath($doc);
-$xpath->registerNamespace("soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
-$xpath->registerNamespace("wsu", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd");
-
-// === Get Body and Timestamp nodes ===
-$bodyNode = $xpath->query('//soapenv:Body')->item(0);
-$timestampNode = $xpath->query('//wsu:Timestamp')->item(0);
-
-// Ensure both have IDs
-$wsuNS = 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd';
-
-$bodyId = $bodyNode->getAttributeNS($wsuNS, 'Id') ?: 'Body';
-$timestampId = $timestampNode->getAttributeNS($wsuNS, 'Id');
-
-if (!$bodyNode->hasAttributeNS($wsuNS, 'Id')) {
-    $bodyNode->setAttributeNS($wsuNS, 'wsu:Id', $bodyId);
-}
-
-
 // === Load private key ===
 $key = new XMLSecurityKey(XMLSecurityKey::RSA_SHA256, ['type' => 'private']);
 $key->loadKey($signingKey, true);
 
-// === Set up DSig ===
-$dsig = new XMLSecurityDSig();
-$dsig->setCanonicalMethod(XMLSecurityDSig::EXC_C14N);
-
-// Sign both Body and Timestamp using SHA256
-$dsig->addReferenceList(
-    [$bodyNode, $timestampNode],
-    XMLSecurityDSig::SHA256,
-    ['http://www.w3.org/2001/10/xml-exc-c14n#'],
-    ['uri' => ['#' . $bodyId, '#' . $timestampId]],
-    ['id_name' => 'Id', 'overwrite' => false]
-);
-
-// Sign and attach to Security header
-$dsig->sign($key);
-$dsig->appendSignature($objWSSE->securityNode, true);
+// === Sign the SOAP message ===
+$key->algorithm = XMLSecurityDSig::SHA256;
+$objWSSE->signSoapDoc($key);
 
 // === Load certificate and attach as BinarySecurityToken ===
 $certContent = file_get_contents($signingCert);
@@ -86,6 +54,8 @@ $token = $objWSSE->addBinaryToken($certContent);
 
 // === Attach BinarySecurityToken using SubjectKeyIdentifier ===
 $objWSSE->attachTokentoSig($token, false, true);
+
+
 
 // === Output signed XML ===
 echo $objWSSE->saveXML();
